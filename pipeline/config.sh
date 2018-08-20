a9b086c33a2714b91b9856127ce67f00a28d832b
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-08-20 10:56:53 -0700

    About to try (from pipeline dir)

--- pipeline/config.sh
+++ pipeline/config.sh
@@ -3,3 +3,5 @@ module load bwa/0.7.17
 module load bedtools/2.27.1
 module load samtools/1.7
 module load mummer/4.0.0
+module load htslib zlib libbz2
+module load gcc
---
+++ test_data/foo.sh
@@ -0,0 +1,1 @@
+../bin/scrub_names.pl cns_p_ctg.fasta cns_h_ctg.fasta> name_mapping.txt
