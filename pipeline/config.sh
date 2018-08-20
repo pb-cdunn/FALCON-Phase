39ec92100cec102972146cdeb994714a9da465e8
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-08-20 11:48:17 -0700

    Works with this

--- pipeline/config.sh
+++ pipeline/config.sh
@@ -1,3 +1,6 @@
+source /mnt/software/Modules/current/init/bash
+module purge
+module load python/3
 module load snakemake
 module load bwa/0.7.17
 module load bedtools/2.27.1
@@ -5,3 +8,5 @@ module load samtools/1.7
 module load mummer/4.0.0
 module load htslib zlib libbz2
 module load gcc
+
+export PYTHONUSERBASE=/scratch/cdunn/repo/sk/.git/LOCAL3
